e28e6579bc85253f3dde070f2fcace06a80dcab5
kcppdevelopers <kcppdevelopers@gmail.com>
2018-10-04 11:10:49 +0530

    Webhooks - Invoice Payment, PPIWP-11

--- includes/class-angelleye-paypal-invoicing-logger.php
+++ includes/class-angelleye-paypal-invoicing-logger.php
@@ -67,11 +67,9 @@ class AngellEYE_PayPal_Invoicing_Logger {
      * @return void
      */
     public function add($handle, $message) {
-        $timestamp = current_time( 'timestamp' );
-        $handle = $handle.$timestamp;
         if ($this->open($handle) && is_resource($this->_handles[$handle])) {
             $time = date_i18n('m-d-Y @ H:i:s -'); // Grab Time
-            @fwrite($this->_handles[$handle.$timestamp], $time . " " . $message . "\n");
+            @fwrite($this->_handles[$handle], $time . " " . $message . "\n");
         }
     }
 
