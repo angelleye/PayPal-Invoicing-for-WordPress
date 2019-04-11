3092b3ede6f86dcb7f0d978741b98cd1b205bde5
kcppdevelopers <kcppdevelopers@gmail.com>
2019-04-11 16:45:54 +0530

    discount array, PPIW-50

--- admin/class-angelleye-paypal-invoicing-request.php
+++ admin/class-angelleye-paypal-invoicing-request.php
@@ -367,14 +367,6 @@ class AngellEYE_PayPal_Invoicing_Request {
                     array(
                         'email_address' => $billing_email,
                         'additional_info_value' => 'add-info',
-                        'phones' => array(
-                            0 =>
-                            array(
-                                'country_code' => $this->angelleye_paypal_invoice_get_phone_country_code($billing_country),
-                                'national_number' => $billing_phone,
-                                'phone_type' => 'MOBILE',
-                            ),
-                        ),
                         'name' => array('given_name' => $billing_first_name, 'surname' => $billing_last_name),
                         'address' => array('address_line_1' => $billing_address_1, 'address_line_1' => $billing_address_2, 'admin_area_2' => $billing_city, 'admin_area_1' => $billing_state, 'postal_code' => $billing_postcode, 'country_code' => $billing_country)
                     ),
